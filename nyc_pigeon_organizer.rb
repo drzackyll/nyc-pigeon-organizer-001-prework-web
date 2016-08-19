4b3deb674f54745bcb4fed69dd9cc0ac1c75a03a
Zack Adams <adams.z.d@gmail.com>
2016-08-19 15:44:31 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,22 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |attribute, detail|
+    detail.each do |type, name_array|
+      name_array.each do |name|
+        pigeon_list[name] = {}
+      end
+    end
+  end
+  pigeon_list.each do |this, that|
+    data.each do |first, second|
+      array = []
+      second.each do |third, fourth|
+        if fourth.include?(this)
+          array.push(third.to_s)
+        end
+        pigeon_list[this][first] = array
+      end
+    end
+  end
+  return pigeon_list
 end
